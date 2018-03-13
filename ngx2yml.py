d03c827d219423beb7c9d699fc2d599b67874c09
Styopa Semenukha <ssemenukha@developmentgateway.org>
2018-03-13 14:06:37 -0400

    Add context class

--- ngx2yml.py
+++ ngx2yml.py
@@ -1,1 +1,11 @@
 #!/usr/bin/python3
+import sys
+
+class Context:
+    def __init__(self, ctx_type, name = None):
+        self.name = name
+        self.ctx_type = ctx_type
+        self.directives = []
+
+    def add_directive(self, directive, args):
+        pass
