ad358c5465dc3b3787443371c42e1b63bdef2a0a
Styopa Semenukha <ssemenukha@developmentgateway.org>
2018-03-13 15:53:42 -0400

    Tokenize lines

--- ngx2yml.py
+++ ngx2yml.py
@@ -1,11 +1,44 @@
 #!/usr/bin/python3
-import sys
+import sys, re
+import pprint
 
 class Context:
-    def __init__(self, ctx_type, name = None):
+    def __init__(self, name, args = None):
+        self.args = args
         self.name = name
-        self.ctx_type = ctx_type
+        self.parent = None
         self.directives = []
+        self.child_lists = {}
 
-    def add_directive(self, directive, args):
+    def add_directive(self, name, args):
         pass
+
+    def add_context(self, child):
+        child.parent = self
+        self.child_lists[child.name].append(child)
+
+# match single or double quoted strings and barewords; strip quotes
+tokenizer = re.compile(r'(?:(?<=\')[^\']*(?=\'))|(?:(?<=")[^"]*(?="))|(?:\([^)]+\))|(?:[^\'"\s]+)')
+
+http = Context('http')
+
+with open(sys.argv[1], 'r') as config:
+    curr_ctx = http
+    for dirty_line in config:
+        line = dirty_line.lstrip().rstrip()
+        if not line or line[0] == '#':
+            # skip blanks and comments
+            continue
+        else:
+            if line[-1] == '}':
+                # exit child context
+                curr_ctx = curr_ctx.parent
+            else:
+                tokens = tokenizer.findall(line[:-1])
+                if line[-1] == ';':
+                    # add directive at current context
+                    curr_ctx.add_directive(name = tokens[0], args = tokens[1:])
+                elif line[-1] == '{':
+                    # add child context
+                    child = Context(name = tokens[0], args = tokens[1:])
+                    curr_ctx.add_context(child)
