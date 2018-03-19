959ca4fedde6c14508462a48ec684d84438c6c69
Styopa Semenukha <ssemenukha@developmentgateway.org>
2018-03-19 17:18:15 -0400

    Generate unique file name

--- splityml.py
+++ splityml.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 # Split YAML stream into site documents
+# Usage: ./splityml.py BIG_YAML OUTPUT_DIR
 
 # Copyright 2018, Development Gateway
 
@@ -14,8 +15,70 @@
 # You should have received a copy of the GNU General Public License along with this program.
 # If not, see <https://www.gnu.org/licenses/>.
 
-import sys, re, logging, yaml, os
+import sys, yaml, os, uuid, logging
 try:
-    from yaml import CDumper as Dumper
+    from yaml import CDumper as Dumper, CBaseLoader as BaseLoader
 except ImportError:
-    from yaml import Dumper
+    from yaml import Dumper, BaseLoader
+
+# configure logging
+valid_levels = ["CRITICAL", "ERROR", "WARNING", "INFO", "DEBUG"]
+try:
+    env_level = os.environ["LOG_LEVEL"]
+    valid_levels.remove(env_level)
+    level = getattr(logging, env_level)
+except KeyError:
+    level = logging.WARNING
+except ValueError:
+    msg = "Expected log level: %s, got: %s. Using default level WARNING." \
+            % ("|".join(valid_levels), env_level)
+    print(msg, file = sys.stderr)
+    level = logging.WARNING
+logging.basicConfig(level = level)
+log = logging.getLogger(sys.argv[0])
+
+unique_names = set()
+with open(sys.argv[1], 'r') as big_file:
+    documents = yaml.load_all(big_file, Loader = BaseLoader)
+    for document in documents:
+        try:
+            server_names = document['site']['server']['server_name']
+            if type(server_names) is str:
+                primary_name = server_names
+            else:
+                primary_name = server_names[0]
+            log.debug('Primary name is %s' % primary_name)
+
+            if primary_name[0] == '.':
+                # strip leading dot
+                primary_name = primary_name[1:]
+                log.debug('Removed leading dot')
+
+            if primary_name in unique_names:
+                log.info('Name %s not unique' % primary_name)
+                listen = document['site']['server']['listen']
+                if type(listen) is list:
+                    # multiple arguments to listen directive
+                    ports = listen
+                else:
+                    ports = [listen]
+
+                for port in listen:
+                    # try appending ports (or other listen args)
+                    new_name = '%s-%s' % (primary_name, port)
+                    if new_name not in unique_names:
+                        # found a unique name
+                        log.info('New unique name %s' % new_name)
+                        primary_name = new_name
+                        break
+
+                if primary_name in unique_names:
+                    # still not unique, resort to UUID
+                    raise KeyError('Can\'t create a unique file name')
+            unique_names.add(primary_name)
+            base_name = primary_name
+        except Exception as e:
+            log.warning(str(e))
+            base_name = str(uuid.uuid4())
+        file_name = os.path.join(sys.argv[2], base_name + '.yml')
+        print(file_name)
